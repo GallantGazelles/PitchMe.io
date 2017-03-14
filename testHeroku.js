7c31266b68ced06f252b967f6e09cbc152c1987d
Alison Zhang <alisonzthu@gmail.com>
2017-03-14 14:39:27 -0700

    test first heroku PR

--- testHeroku.js
+++ testHeroku.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
-
+//for first PR test
 var app = express();
 app.get('/', function(req, res) {
 	res.send("HELlO WORLD");
 });
-
-app.listen(3000, function() {
+//with process.env.PORT, type in the terminal: export PORT=1234, we can re-run the app on port 1234
+app.listen(process.env.PORT, function() {
 	console.log("listening to 3000");
 });
