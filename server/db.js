24b5fb37c00434a2ec555a79c4b155dfa8312fdf
Alison Zhang <alisonzthu@gmail.com>
2017-03-15 14:51:00 -0700

    starting to write db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,18 @@
-db.js
+var pg = require('pg');
+
+var client = new pg.Client();
+
+client.connect(function(err) {
+	if(err) throw err;
+//fill in the query string:
+	client.query('SELECT $1::text as name', ['brianc'], function (err, result) {
+    if (err) throw err;
+    console.log(result.rows[0]);
+    client.end(function(err) {
+    	if(err) throw err;
+    });
+});
+
+
+
+});
