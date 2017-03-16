a7798466b7fba334a016c79ab8d476af65cd682b
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-15 17:08:51 -0700

    FEAT: Add Sign In form

---
+++ client/src/components/SignIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SignIn = () => (
+  <section>
+    <div>
+      Sign In
+      <input type="text" name="name" placeholder="Name" />
+      <input type="text" name="email" placeholder="Email" />
+      <input type="text" name="re-enter email" placeholder="Re-enter Email" />
+      <input type="text" name="password" placeholder="Password" />
+      <input type="text" name="re-enter password" placeholder="Re-enter Password" />
+      <div>Sign Up! button</div>
+      <p>Or</p>
+      <div>Log in with Facebook button</div>
+    </div>
+  </section>
+)
+
+export default SignIn;
