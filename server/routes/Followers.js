3aae04aac3bcf9aa8df6966f229f654b3c508434
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-19 16:47:13 -0700

    FIX: comments

--- server/routes/Followers.js
+++ server/routes/Followers.js
@@ -6,17 +6,17 @@ module.exports.getFollowers = (req, res, next) => {
   const userId = req.query.userId;
 
   if (q === 'count' && pitchId) {
-    // /api/followers?p=count&pitchId=1234
+    // Example: /api/followers?q=count&pitchId=1234
     db.getPitchFollowerCount(pitchId)
       .then(results => res.send(results.rows))
       .catch(error => res.send(error));
   } else if (q === 'follows') {
-    // /api/followers?p=followst&pitchId=1234
+    // Example: /api/followers?q=followst&pitchId=1234
     db.getPitchFollowers(pitchId)
       .then(results => res.send(results.rows))
       .catch(error => res.send(error));
   } else if (userId) {
-    // /api/followers?userId=5678
+    // Example: /api/followers?userId=5678
     db.getUserPitchFollows(userId)
       .then(results => res.send(results.rows))
       .catch(error => res.send(error));
