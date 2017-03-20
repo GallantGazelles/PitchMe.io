51ad727f5302dca8ac790e415d82b35b4ce8b4dc
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-20 15:16:37 -0700

    Add title/logo column and icon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
 
-const NavBar = (props) => (
-  <nav>
-    <div>
-      <span>Home </span>
-      <span>Start a Pitch </span>
-      <span>How it works </span>
-      <span>Companies </span>
-      <span>Sign in </span>
-      <span>Sign up </span>
-    </div>
-    <div>
-      <input type="text" name="search" placeholder="Search" />
-    </div>
-  </nav>
+export default props => (
+  <Grid padded>
+    <Grid.Column>
+      <Header as="h1">
+        <Icon name='meh' />PitchMe
+      </Header>
+    </Grid.Column>
+  </Grid>
 )
-
-export default NavBar;
