5f06c55507794d155beedcf1d33b2ac36ad17eef
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-03-21 15:28:30 -0700

    Minor additions to redux

--- redux/src/components/Votes.jsx
+++ redux/src/components/Votes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+//will need to take in two dispatches - upvote & downvote
+//will need to take in one state - vote count
 const Votes = () => (
   <div>
     <div>VOTES: 1234</div>
--- redux/src/index.jsx
+++ redux/src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import App from './components/App.jsx';
+
+// const store = createStore()
 
 ReactDOM.render(<App />, document.getElementById('app'))
