8215b141f76cb3942002d424ae6d8ad336f57fd0
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-21 23:06:43 -0700

    Complete pitch pic and votes

--- client/src/components/Pitch.jsx
+++ client/src/components/Pitch.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import Video from './Video.jsx';
-import { Container, Divider, Grid, Header, Icon, Image, Item, Label, Segment, Statistic } from 'semantic-ui-react';
+import { Button, Container, Divider, Grid, Header, Icon, Image, Item, Label, Segment, Statistic } from 'semantic-ui-react';
 
 export default () => (
   <Segment basic>
     <Video />
     <Container text>
-      <Segment basic padded>
-        <Item>
-          <Item.Image floated='left' size='small' src='https://ph-files.imgix.net/8a2b7acf-d24d-46f3-9060-723db65625a9?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2' />
-
-          <Item.Content>
-            <Item.Header as='h1'>Shift</Item.Header>
-            <Item.Meta>
-              <span className='blurb'>Switch between Gmail accounts like a boss.</span>
-            </Item.Meta>
-            <Item.Description>Navigate between Mail, Calendar and Drive easily, for all your accounts.</Item.Description>
-            <Item.Extra>
-              <Icon color='green' name='check' /> 1609 Votes
-            </Item.Extra>
-            <Item.Extra>
-              <Label>Tech</Label>
-              <Label>iPhone</Label>
-            </Item.Extra>
-          </Item.Content>
-        </Item>
-      </Segment>
-
+      <Grid padded columns={2}>
+        <Grid.Column width={5}>
+          <Image centered size='small' src='https://ph-files.imgix.net/8a2b7acf-d24d-46f3-9060-723db65625a9?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2' />
+          <Segment basic textAlign='center'>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Item.Header as='a'><Icon color='green' name='check' />1609 Votes</Item.Header>
+              </Item.Content>
+            </Item>
+            <Item>
+              <Button icon size='big'><Icon name='arrow up' /></Button>
+              <Button icon size='big'><Icon name='arrow down' /></Button>
+            </Item>
+          </Item.Group>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column width={11}>
+          <Header as='h1'>
+            Shift
+            <Header.Subheader>
+              Switch between Gmail accounts like a boss
+            </Header.Subheader>
+          </Header>
+          <p>Spicy jalapeno bacon ipsum dolor amet jowl cow ribeye corned beef. Pastrami tongue meatloaf chuck, bresaola pig strip steak andouille corned beef kielbasa brisket.</p>
+          <p>Spicy jalapeno bacon ipsum dolor amet jowl cow ribeye corned beef. Pastrami tongue meatloaf chuck, bresaola pig strip steak andouille corned beef kielbasa brisket.</p>
+          <p><Label>Tech</Label><Label>iPhone</Label></p>
+        </Grid.Column>
+      </Grid>
     </Container>
 
     <Divider horizontal>
