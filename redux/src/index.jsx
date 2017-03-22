c03d360553cee6472a795f36e3968872972f1939
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-03-22 12:29:41 -0700

    Renamed containers to reducers and added votes

--- redux/src/index.jsx
+++ redux/src/index.jsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import App from './components/App.jsx';
 
-// const store = createStore()
-
+// Need to provide 
 ReactDOM.render(<App />, document.getElementById('app'))
