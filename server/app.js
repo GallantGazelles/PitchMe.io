adeb09dc4634597908663c58a9fb8b1b9e1ded64
Alison Zhang <alisonzthu@gmail.com>
2017-03-22 13:47:32 -0700

    using postgresqlStore

--- server/app.js
+++ server/app.js
@@ -3,10 +3,46 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const pg = require('pg');
 const db = require('./db.js');
-
+const session = require('express-session');
+const PostgreSqlStore = require('connect-pg-simple')(session);
 const app = express();
 const router = require('./routes.js');
+const SessionTAB = require('./routes/Sessions');
 
+let sessionOptions = {
+	secret: "secret",
+	name: 'pitchme.io', //TBD: is this the key for cookie?
+	resave: true,
+	cookie: {maxAge: 30 * 24 * 60 * 60 * 1000},
+	saveUninitialized: false,
+	store: new PostgreSqlStore({
+		conString: 'postgres://localhost:5432/ggdb',
+	    pg: pg,
+	    tableName: 'sessions'})
+	// store: new PostgreSqlStore({conString: 'postgres://postgres:postgres@localhost:5433/postgres'
+    // })
+};
+app.use(session(sessionOptions));
+app.get('/', function(req, res) {
+	let sess = req.session;
+    if(sess.name === 'pitchme.io') {
+    	//resume session, get all the user data
+
+    } else {
+    	//assign new session.
+    	let s = SessionTAB.assignSession();
+    	console.log('sessionid is: ', s);
+    	//how to manipulate cookie in res?
+    	console.log('response: ', res);
+ 
+    	//send back cookie with the key of 'pitchme.io'
+    }
+	// console.log(req);
+	// console.log(req.session);
+	// //find out the sessionID
+	// console.log('res sessionID: ', res.req.sessionID);
+	res.end('bye');
+});
 app.use(express.static(path.join(__dirname, '/../client/')));
 app.use(bodyParser.json());
 
---
+++ server/util.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto');
+
+
+module.exports.createHash = (data, salt) => {
+	return crypto.createHash('sha1').update(data + salt).digest('hex');
+};
+
+module.exports.createSalt = () => {
+	return crypto.randomBytes(20).toString('hex');
+};
+
+
+
