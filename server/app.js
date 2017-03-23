b89c8ddc64bf47c63142c27c21e771e38b523aac
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-23 11:28:33 -0700

    Add companies page and route

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const app = express();
 const router = require('./routes.js');
 
 app.use('/', express.static(path.join(__dirname, '/../client/')));
+app.use('/companies', express.static(path.join(__dirname, '/../client/')));
 app.use('/pitch', express.static(path.join(__dirname, '/../client/')));
 app.use('/signup', express.static(path.join(__dirname, '/../client/')));
 app.use('/signin', express.static(path.join(__dirname, '/../client/')));
