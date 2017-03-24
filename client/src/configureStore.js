e28d71c05749501a8d484a50f3c533de68f08602
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-03-24 16:05:44 -0700

    Fixes store spelling

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,13 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import rootReducers from './reducers/rootReducers';
+import rootReducer from './reducers/rootReducers';
 
 const middleware = applyMiddleware(thunkMiddleware, createLogger())
 
 export default function configureStore(preloadedState) {
   return createStore(
-    rootReducers,
+    rootReducer,
     preloadedState,
     middleware
   )
