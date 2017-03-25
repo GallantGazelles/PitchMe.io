9f13c9dc2f1751b251b33cb7a1b43651cb8f9cbc
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-03-24 17:13:57 -0700

    Adds ability to pull connect from server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,24 +4,29 @@ import MainPitchInfo from './MainPitchInfo.jsx';
 import TrendingVideos from './TrendingVideos.jsx';
 import axios from 'axios';
 import { Container, Divider } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { fetchPitches } from '../actions/pitch';
 
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      pitches: []
-    }
+    // this.state = {
+    //   pitches: []
+    // }
   }
 
   componentDidMount() {
     // Get all pitches. Filter when they are returned.
-    axios.get('http://localhost:8080/api/pitches?q=all')
-         .then(response => {
-            console.dir(response.data);
-            this.setState({ pitches: response.data });
-         })
-         .catch(error => console.error(error));
+    // axios.get('http://localhost:8080/api/pitches?q=all')
+    //      .then(response => {
+    //         console.dir(response.data);
+    //         this.setState({ pitches: response.data });
+    //      })
+    //      .catch(error => console.error(error));
+    const {dispatch} = this.props;
+    console.log(this.props)
+    dispatch(fetchPitches())
   }
 
   render() {
@@ -36,3 +41,13 @@ export default class App extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    pitches: state.pitches.pitches,
+    mainPitch: state.pitches.mainPitch
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
