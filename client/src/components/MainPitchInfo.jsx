c5342f324ac003c5f378c668f6f77f52e60229b9
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-26 14:45:59 -0700

    Update colors for Main Page votes and begin adding backend DB logic for votes

--- client/src/components/MainPitchInfo.jsx
+++ client/src/components/MainPitchInfo.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { upvote, downvote } from '../actions/pitch';
+import { connect } from 'react-redux';
 import { Button, Container, Header, Icon, Item, Label, Segment, Statistic } from 'semantic-ui-react';
 
-export default () => (
-  <Container textAlign='center' text>
-    <Item.Group>
-      <Item>
-        <Item.Content>
-          <Header as="h1">Shift</Header>
-          <Item.Description>
-            <p>Switch between Gmail accounts like a boss.</p>
-          </Item.Description>
-        </Item.Content>
-      </Item>
-      <Item>
-        <Item.Content>
-          <Item.Header as='h1'><Icon color='green' name='check' />1609 Votes</Item.Header>
-          <Segment basic textAlign='center'>
-            <Button icon size='big'><Icon name='arrow up' /></Button>
-            <Button icon size='big'><Icon name='arrow down' /></Button>
-          </Segment>
-        </Item.Content>
-      </Item>
-    </Item.Group>
-  </Container>
-)
+export default class MainPitchInfo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      vote_type: -1
+    }
+
+    this.handleClick = (value) => this.setState({
+      active: value
+    })
+  }
+
+  render() {
+
+
+    return (
+      <Container textAlign='center' text>
+        <Item.Group>
+          <Item>
+            <Item.Content>
+              <Header as="h1">Shift</Header>
+              <Item.Description>
+                <p>Switch between Gmail accounts like a boss.</p>
+              </Item.Description>
+            </Item.Content>
+          </Item>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h1'><Icon color='green' name='check' />1609 Votes</Item.Header>
+              <Segment basic textAlign='center'>
+                <Button icon size='big' color={this.state.vote_type === 1 ? 'green' : 'blue'} >
+                  <Icon name='arrow up' />
+                </Button>
+                <Button icon size='big' color={this.state.vote_type === -1 ? 'red' : 'blue'}>
+                  <Icon name='arrow down' />
+                </Button>
+              </Segment>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Container>
+    )
+  }
+}
