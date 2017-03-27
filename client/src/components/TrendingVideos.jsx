edd4ded8df9e68857c2e372a94ac31cc581fd3bf
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-26 20:51:17 -0700

    Fix trending pitches cards

--- client/src/components/TrendingVideos.jsx
+++ client/src/components/TrendingVideos.jsx
@@ -3,40 +3,48 @@ import TrendingVideoCard from './TrendingVideoCard.jsx'
 import { Card, Divider, Grid, Header, Icon, Image } from 'semantic-ui-react';
 
 
-export default () => (
-  <section>
-    <Divider horizontal>
-      <Header as='h4'>
-        <Icon name='line chart' />
-        Trending Pitches
-      </Header>
-    </Divider>
-    <Divider hidden />
+export default (props) => {
+  if (props.pitches.length > 0) {
+    return (
+      <section>
+        <Divider horizontal>
+          <Header as='h4'>
+            <Icon name='line chart' />
+            Trending Pitches
+          </Header>
+        </Divider>
+        <Divider hidden />
 
-    <Grid container padded columns={3}>
-      <Grid.Row>
-        <Grid.Column>
-          <TrendingVideoCard />
-        </Grid.Column>
-        <Grid.Column>
-          <TrendingVideoCard />
-        </Grid.Column>
-        <Grid.Column>
-          <TrendingVideoCard />
-        </Grid.Column>
-      </Grid.Row>
+        <Grid container padded columns={3}>
+          <Grid.Row>
+            <Grid.Column>
+              <TrendingVideoCard pitch={props.pitches[0]} />
+            </Grid.Column>
+            <Grid.Column>
+              <TrendingVideoCard pitch={props.pitches[1]} />
+            </Grid.Column>
+            <Grid.Column>
+              <TrendingVideoCard pitch={props.pitches[2]} />
+            </Grid.Column>
+          </Grid.Row>
 
-      <Grid.Row>
-        <Grid.Column>
-          <TrendingVideoCard />
-        </Grid.Column>
-        <Grid.Column>
-          <TrendingVideoCard />
-        </Grid.Column>
-        <Grid.Column>
-          <TrendingVideoCard />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </section>
-)
+          <Grid.Row>
+            <Grid.Column>
+              <TrendingVideoCard pitch={props.pitches[3]} />
+            </Grid.Column>
+            <Grid.Column>
+              <TrendingVideoCard pitch={props.pitches[4]} />
+            </Grid.Column>
+            <Grid.Column>
+              <TrendingVideoCard pitch={props.pitches[5]} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </section>
+    )
+  } else {
+    return (
+      <div>NOPE!</div>
+    )
+  }
+}
