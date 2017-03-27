e996bfe0102b04c500e32a728e9ccbfbc5539f2d
Craig Rodrigues <PagingCraig@gmail.com>
2017-03-27 12:18:00 -0700

    Add route for individual pitch page query

--- client/src/components/Pitch.jsx
+++ client/src/components/Pitch.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { upvote, downvote } from '../actions/pitch';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import Video from './Video.jsx';
 import Discussion from './Discussion.jsx';
-import { Button, Container, Divider, Grid, Header, Icon, Image, Item, Label, Segment, Statistic } from 'semantic-ui-react';
+import {
+  Button, Container, Dimmer, Divider, Grid, Header, Icon, Image, Item, Label, Loader, Segment, Statistic
+} from 'semantic-ui-react';
 
 class Pitch extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
+  }
+
+  componentWillReceiveProps() {
+    let pitchId = this.props.match.params.pitchId;
+    if (pitchId) {
+      axios.get('http://localhost:8080/api/pitch', {params: {pitchId: pitchId}})
+        .then(results => {
+          this.setState({pitch: results.data[0]});
+        })
+        .catch(error => console.log(error))
+    }
   }
 
   render() {
-    console.log(this.props);
     const {user, id, vote_type} = this.props
     const {onClickUpvote, onClickDownvote} = this.props
     const upvoteButton = <Button icon size='big' color='green' onClick={() => onClickUpvote(user, id, vote_type)}><Icon name='arrow up' /></Button>
@@ -19,46 +33,58 @@ class Pitch extends Component {
     const neutralUpButton = (<Button icon basic size='big' color='grey' onClick={() => onClickUpvote(user, id, vote_type)}><Icon name='arrow up' /></Button>)
     const neutralDownButton = <Button icon basic size='big' color='grey' onClick={() => onClickDownvote(user, id, vote_type)}><Icon name='arrow down' /></Button>
 
-    return (
-      <Segment basic>
-        <Video />
-        <Container text>
-          <Grid padded columns={2}>
-            <Grid.Column width={6}>
-              <Image centered size='small' src='https://ph-files.imgix.net/8a2b7acf-d24d-46f3-9060-723db65625a9?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2' />
-              <Segment basic textAlign='center'>
-                <Item>
-                  <Item.Content>
-                    <Item.Header as='h2'><Icon color='green' name='check' />1609 Votes</Item.Header>
-                    { user !== null ? (this.props.vote_type === 1 ? upvoteButton : neutralUpButton):<div></div> }
-                    { user !== null ? (this.props.vote_type === -1 ? downvoteButton : neutralDownButton):<div></div> }
-                  </Item.Content>
-                </Item>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column width={10}>
-              <Header as='h1'>
-                Shift <Label>Tech</Label>
-                <Header.Subheader>
-                  Switch between Gmail accounts like a boss
-                </Header.Subheader>
-              </Header>
-              <p>Spicy jalapeno bacon ipsum dolor amet jowl cow ribeye corned beef. Pastrami tongue meatloaf chuck, bresaola pig strip steak andouille corned beef kielbasa brisket.</p>
-              <p>Spicy jalapeno bacon ipsum dolor amet jowl cow ribeye corned beef. Pastrami tongue meatloaf chuck, bresaola pig strip steak andouille corned beef kielbasa brisket.</p>
-              <p>
-                <Button primary>Follow</Button>
-                <Button primary>Visit Website</Button>
-                <Button primary>Invest</Button>
-                <Button primary><Icon name='share' />  Share</Button>
-              </p>
-            </Grid.Column>
-          </Grid>
+
+    if (this.state.pitch) {
+      return (
+        <Segment basic>
+          <Video />
+          <Container text>
+            <Grid padded columns={2}>
+              <Grid.Column width={6}>
+                <Image centered size='small' src='https://ph-files.imgix.net/8a2b7acf-d24d-46f3-9060-723db65625a9?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2' />
+                <Segment basic textAlign='center'>
+                  <Item>
+                    <Item.Content>
+                      <Item.Header as='h2'><Icon color='green' name='check' />{this.state.pitch.votes} Votes</Item.Header>
+                      { user !== null ? (this.props.vote_type === 1 ? upvoteButton : neutralUpButton):<div></div> }
+                      { user !== null ? (this.props.vote_type === -1 ? downvoteButton : neutralDownButton):<div></div> }
+                    </Item.Content>
+                  </Item>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={10} >
+                <Header as='h1'>
+                  {this.state.pitch.name} <Label basic color='blue'>Tech</Label>
+                  <Header.Subheader>
+                    {this.state.pitch.blurb}
+                  </Header.Subheader>
+                </Header>
+                <p>Spicy jalapeno bacon ipsum dolor amet jowl cow ribeye corned beef. Pastrami tongue meatloaf chuck, bresaola pig strip steak andouille corned beef kielbasa brisket.</p>
+                <p>Spicy jalapeno bacon ipsum dolor amet jowl cow ribeye corned beef. Pastrami tongue meatloaf chuck, bresaola pig strip steak andouille corned beef kielbasa brisket.</p>
+                <p>
+                  <Button primary>Follow</Button>
+                  <Button primary>Visit Website</Button>
+                  <Button primary>Invest</Button>
+                  <Button primary><Icon name='share' />  Share</Button>
+                </p>
+              </Grid.Column>
+            </Grid>
+          </Container>
+
+          <Discussion />
+
+        </Segment>
+      )
+    } else {
+      return (
+        <Container>
+          <Dimmer active inverted>
+            <Loader size='large'>Loading</Loader>
+          </Dimmer>
+          LOADING
         </Container>
-
-        <Discussion />
-
-      </Segment>
-    )
+      )
+    }
   }
 }
 
